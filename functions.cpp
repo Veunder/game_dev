a608953d761fc133e71586219770843df9d4dc02
haku <andrejosipov19120@gmail.com>
2023-08-08 12:30:56 +0300

    Добавили инвентарь

--- functions.cpp
+++ functions.cpp
@@ -25,11 +25,11 @@ enum class CellType {
     Operative
 };
 
-char Creature::STATIC_ID = 'A';
+char Creature::Creature_ID = 'A';
 
 Creature::Creature(Point point) {
-    char_id = STATIC_ID;
-    STATIC_ID++;
+    char_id = Creature_ID;
+    Creature_ID++;
     pos = point;
 }
 
@@ -37,32 +37,28 @@ char Creature::get_char_id() const {
     return char_id;
 }
 
-Operative::Operative(Point point) : Creature(point) {
-    char_id = Op_ID;
-    Op_ID++;
+Operative::Operative(Point point1, Point point) : Creature(point1) {
+    char_id = Operative_ID;
+    Operative_ID++;
     pos = point;
 }
 
-char Operative::Op_ID = 'a';
+char Operative::Operative_ID = 'a';
 
 char Operative::get_char_id() const {
     return char_id;
 }
 
 int Operative::move(char key) {
-    if (key == 'u'){ //up
+    if (key == 'u') { //up
 
-    }
-    else if (key == 'd'){ // down
+    } else if (key == 'd') { // down
 
-    }
-    else if (key == 'r'){ // right
+    } else if (key == 'r') { // right
 
-    }
-    else if (key == 'l'){ // left
+    } else if (key == 'l') { // left
 
-    }
-    else { return 1; }
+    } else { return 1; }
     return 0;
 }
 
@@ -112,7 +108,7 @@ AidKit::AidKit() {
     weight = 5;
 }
 
-Weapon::Weapon(WeaponType wt_) : AmmoContainer(wt_){
+Weapon::Weapon(WeaponType wt_) : AmmoContainer(wt_) {
     if (wt == WeaponType::Pistol) {
         damage = 15;
         fireTime = 1;
@@ -138,7 +134,7 @@ Weapon::Weapon(WeaponType wt_) : AmmoContainer(wt_){
     }
 }
 
-void Weapon::get_info () {
+void Weapon::get_info() {
     cout << "WeaponType " << enumToString(wt) << endl
          << "currentAmmo " << currentAmmo << endl
          << "maxAmmo " << maxAmmo << endl
@@ -147,22 +143,27 @@ void Weapon::get_info () {
          << "reloadTime " << reloadTime << endl;
 }
 
-void Cell::set_type (CellType cellType) {
+void Cell::set_type(CellType cellType) {
     ct = cellType;
 }
-void Cell::set_operative (Operative *op_) {
+
+void Cell::set_operative(Operative *op_) {
     op = op_;
 }
-void Cell::set_creature (Creature *creature_) {
+
+void Cell::set_creature(Creature *creature_) {
     creature = creature_;
 }
-void Cell::set_weapon (Weapon *weapon_) {
+
+void Cell::set_weapon(Weapon *weapon_) {
     weapon = weapon_;
 }
-void Cell::set_ammoContainer (AmmoContainer *ammoContainer_) {
+
+void Cell::set_ammoContainer(AmmoContainer *ammoContainer_) {
     ammoContainer = ammoContainer_;
 }
-void Cell::set_aidKit (AidKit *aidKit_) {
+
+void Cell::set_aidKit(AidKit *aidKit_) {
     aidKit = aidKit_;
 }
 
@@ -198,9 +199,8 @@ Level::Level(std::string &&file_name) {
             }
         }
         InputFile.close();
-    }
-    else {
-        if (file_name == "default_map.txt"){
+    } else {
+        if (file_name == "default_map.txt") {
             cout << "Не удалось найти дефолтный уровень, выход." << endl;
             exit(0);
         }
@@ -215,43 +215,35 @@ int Level::set_cell(int i, int j, char c, Operative *op, Creature *creature, Wea
     cells[i][j].set_symbol(c);
     if (c == '#') {
         cells[i][j].set_type(CellType::Wall);
-    }
-    else if (c == '=') {
+    } else if (c == '=') {
         cells[i][j].set_type(CellType::Partition);
-    }
-    else if (c == '-') {
+    } else if (c == '-') {
         cells[i][j].set_type(CellType::Glass);
-    }
-    else if (c >= 'a' && c <= 'z') {
+    } else if (c >= 'a' && c <= 'z') {
         if (!op) { op = new Operative(Point(), Point()); }
         cells[i][j].set_type(CellType::Operative);
         cells[i][j].set_operative(op);
         ops.push_back(op);
         cells[i][j].set_symbol(op->get_char_id());
-    }
-    else if (c >= 'A' && c <= 'Z') {
+    } else if (c >= 'A' && c <= 'Z') {
         if (!creature) { creature = new Creature; }
         cells[i][j].set_type(CellType::Creature);
         cells[i][j].set_creature(creature);
         creatures.push_back(creature);
         cells[i][j].set_symbol(creature->get_char_id());
-    }
-    else if (c == ')'){
+    } else if (c == ')') {
         if (!ammoContainer) { ammoContainer = new AmmoContainer(WeaponType::Rifle); } // Сделать создание рандомным
         cells[i][j].set_type(CellType::AmmoContainer);
         cells[i][j].set_ammoContainer(ammoContainer);
-    }
-    else if (c == '+') {
-        if (!aidKit) {aidKit = new AidKit; }
+    } else if (c == '+') {
+        if (!aidKit) { aidKit = new AidKit; }
         cells[i][j].set_type(CellType::AidKit);
         cells[i][j].set_aidKit(aidKit);
-    }
-    else if (c == '$') {
+    } else if (c == '$') {
         if (!weapon) { weapon = new Weapon(WeaponType::Rifle); }
         cells[i][j].set_type(CellType::Weapon);
         cells[i][j].set_weapon(weapon);
-    }
-    else{
+    } else {
         cells[i][j].set_type(CellType::Empty);
         cells[i][j].set_symbol('.');
     }
@@ -259,10 +251,29 @@ int Level::set_cell(int i, int j, char c, Operative *op, Creature *creature, Wea
 }
 
 void Level::PrintLevel() {
-    for (const auto& i : cells){
-        for (auto  j : i){
+    for (const auto &i: cells) {
+        for (auto j: i) {
             cout << j.get_symbol();
         }
         cout << endl;
     }
+}
+
+Weapon Inventory::get_weapon() {
+    for (int i = 0; i < weapons.size(); ++i) {
+        cout << i + 1 << '.' << endl;
+        weapons[i]->get_info();
+    }
+    int choice;
+    while (!(cin >> choice).good() || choice < 1 || choice > weapons.size()) {
+        cout << "Incorrect number!\n";
+        cin.clear();
+        cin.ignore(100, '\n');
+    }
+    cin.clear();
+    cin.ignore(100, '\n');
+    choice--;
+    Weapon *return_value = weapons[choice];
+    weapons.erase(weapons.cbegin() + choice);
+    return *(return_value);
 }
--- functions.h
+++ functions.h
@@ -11,6 +11,10 @@ enum class CellType;
 
 class Weapon;
 
+class AmmoContainer;
+
+class Inventory;
+
 struct Point {
     int x;
     int y;
@@ -18,20 +22,22 @@ struct Point {
 
 class Creature {
 protected:
-    static char STATIC_ID;
+    static char Creature_ID;
     char char_id;
     Point pos;
+    Inventory *inventory;
 public:
     explicit Creature(Point point);
+
     [[nodiscard]] char get_char_id() const;
 };
 
-class Operative : Creature{
+class Operative : Creature {
 protected:
-    Weapon *weapon{};
+    static char Operative_ID;
 public:
-    explicit Operative(Point point);
-    [[nodiscard]] char get_char_id() const;
+    explicit Operative(Point point1, Point point);
+
     int move(char key);
 };
 
@@ -43,7 +49,7 @@ protected:
     int currentAmmo;
     int maxAmmo;
 public:
-    explicit AmmoContainer (WeaponType wt_);
+    explicit AmmoContainer(WeaponType wt_);
 };
 
 class AidKit {
@@ -61,26 +67,42 @@ private:
     int reloadTime;
 public:
     explicit Weapon(WeaponType wt_);
-    void get_info ();
+
+    void get_info();
+};
+
+class Inventory {
+protected:
+    vector<Weapon *> weapons{};
+    vector<AmmoContainer *> ammoContainers{};
+    vector<AidKit *> aidKits{};
+public:
+    Weapon get_weapon();
 };
 
 class Cell {
 private:
     char symbol;
     CellType ct;
-    Operative * op;
-    Creature * creature;
+    Operative *op;
+    Creature *creature;
     Weapon *weapon;
     AmmoContainer *ammoContainer;
     AidKit *aidKit;
 public:
     void set_symbol(char c);
-    void set_type (CellType cellType);
-    void set_operative (Operative *op_);
-    void set_creature (Creature *creature_);
-    void set_weapon (Weapon *weapon_);
-    void set_ammoContainer (AmmoContainer *ammoContainer_);
-    void set_aidKit (AidKit *aidKit_);
+
+    void set_type(CellType cellType);
+
+    void set_operative(Operative *op_);
+
+    void set_creature(Creature *creature_);
+
+    void set_weapon(Weapon *weapon_);
+
+    void set_ammoContainer(AmmoContainer *ammoContainer_);
+
+    void set_aidKit(AidKit *aidKit_);
 
     [[nodiscard]] char get_symbol() const;
 
@@ -94,11 +116,13 @@ private:
     vector<Operative *> ops;
     vector<Creature *> creatures;
 public:
-    explicit Level (string &&file_name);
+    explicit Level(string &&file_name);
+
     void PrintLevel();
 
-    [[nodiscard]] int get_width () const { return width; }
-    [[nodiscard]] int get_height () const { return height; }
+    [[nodiscard]] int get_width() const { return width; }
+
+    [[nodiscard]] int get_height() const { return height; }
 
     /*
     1. . - пустая клетка пола;
@@ -111,8 +135,8 @@ public:
     8. + - аптечка;
     9. $ - оружие;
     */
-    int set_cell (int i, int j, char c, Operative * op = nullptr, Creature * creature = nullptr,
-                  Weapon *weapon = nullptr, AmmoContainer *ammoContainer = nullptr, AidKit *aidKit = nullptr);
+    int set_cell(int i, int j, char c, Operative *op = nullptr, Creature *creature = nullptr,
+                 Weapon *weapon = nullptr, AmmoContainer *ammoContainer = nullptr, AidKit *aidKit = nullptr);
 };
 
 #endif
